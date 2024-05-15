87f12376633d56ec715c5fd490828582f9f20183
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-05-15 12:41:24 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import RippleEffect from "@/components/RippleEffect";
 export default function Home() {
   return (
     <>
-      <div>
-        <RippleEffect />
-        <div style={{ color: "white" }}>
-          <h1>
-            Line break
-          </h1>
-          Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Aut iure accusantium voluptatibus, exercitationem placeat in. Nulla quo saepe aut esse excepturi doloribus veniam voluptatibus iusto. Repudiandae ipsam atque nisi quia.
-        </div>
-        <RippleEffect />
+      <RippleEffect />
+
+      <div style={{ color: "white" }}>
+        <h1>
+          Line break
+        </h1>
+        Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Aut iure accusantium voluptatibus, exercitationem placeat in. Nulla quo saepe aut esse excepturi doloribus veniam voluptatibus iusto. Repudiandae ipsam atque nisi quia.
       </div>
+
+      <RippleEffect />
     </>
   );
 }
